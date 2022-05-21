677e36b56b8fe9824a115ad5afb716853ca442d2
Rostislav Statko <rostislav.statko@gmail.com>
2022-05-21 12:23:21 +0300

    feat: add ts support

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-"use strict";
-
 import vertexShaderSource from './vertex-shader.vert'
 import fragmentShaderSource from './fragment-shader.frag'
 
-/**
- * 
- * @param {WebGL2RenderingContext} gl 
- * @param {number} type 
- * @param {string} source 
- */
-function createShader(gl, type, source) {
+function createShader(gl: WebGL2RenderingContext, type: number, source: string) {
   const shader = gl.createShader(type)
+
+  if (!shader) {
+    return null;
+  }
+
   gl.shaderSource(shader, source)
   gl.compileShader(shader)
   const isSuccess = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
@@ -19,16 +16,15 @@ function createShader(gl, type, source) {
   }
   console.log(gl.getShaderInfoLog(shader))
   gl.deleteShader(shader)
+  return null;
 }
 
-/**
- * 
- * @param {WebGL2RenderingContext} gl 
- * @param {WebGLShader} vertexShader 
- * @param {WebGLShader} fragmentShader 
- */
-function createProgram(gl, vertexShader, fragmentShader) {
+function createProgram(gl: WebGL2RenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
   const program = gl.createProgram()
+
+  if (!program)
+    return null;
+
   gl.attachShader(program, vertexShader)
   gl.attachShader(program, fragmentShader)
   gl.linkProgram(program)
@@ -39,11 +35,13 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
   console.log(gl.getProgramInfoLog(program))
   gl.deleteProgram(program)
+  return null;
 }
 
 function main() {
   // Get A WebGL context
   const canvas = document.createElement('canvas')
+
   const root = document.getElementById('root')
 
   if (root === null)
@@ -59,7 +57,15 @@ function main() {
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 
+  if (vertexShader === null || fragmentShader === null){
+    return;
+  }
+
   const program = createProgram(gl, vertexShader, fragmentShader)
+
+  if (program === null){
+    return;
+  }
 
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position")
   const positionBuffer = gl.createBuffer()
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 * @type {import('webpack').Configuration}
 */
 const webpackConfig = {
-  entry: './src/index.js',
-  devtool: 'source-map',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
@@ -23,8 +23,15 @@ const webpackConfig = {
   mode: 'development',
   module: {
   	rules: [
-      { test: /\.(html)$/, use: ['html-loader'] },
-      { test: /\.(glsl|vert|frag)$/, type: 'asset/source', },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      { 
+        test: /\.(glsl|vert|frag)$/,
+        type: 'asset/source',
+      },
     ],
   }
 }
